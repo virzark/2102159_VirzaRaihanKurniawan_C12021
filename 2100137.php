564fd85ed892a6f754c841a7a9bfd672be22b6e7
mnyasin26 <forsvive22@gmail.com>
2022-03-10 22:56:35 +0700

    adding yasin's file

---
+++ 2100137.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>This is Yasin's PHP page</h1>
+
+<?php
+echo "Hello World!<br/>";
+echo "My name is Muhamad Nur Yasin Amadudin<br/>";
+echo "I have my own NIM which is 2100137";
+echo "kita coba";
+echo "kita coba";
+echo "kita coba";
+
+?>
+
+</body>
+</html>
