ada27f85d9913b413ee878d54c0b705cccc94dd6
virzark <virza.r.kurniawan@gmail.com>
2022-03-10 23:12:53 +0700

    test

--- 2102159.php
+++ 2102159.php
@@ -1,2 +1,15 @@
-Nama : Virza Raihan Kurniawan
-NIM : 2102159
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>This is Virza page</h1>
+
+<?php
+echo "Hello World!<br/>";
+echo "My name is Virza Raihan Kurniawan<br/>";
+echo "I have my own NIM which is 2102159";
+echo "Jangan lupa makan";
+?>
+
+</body>
+</html>
